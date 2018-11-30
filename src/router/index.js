3a9671addcc10ec4887de59179e1f2ff2d33b9f2
nahetian <wlgou6363@qq.com>
2018-11-30 16:13:23 +0800

    去掉#

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Home from '@/components/Home'
 import Details from '@/components/Details'
 Vue.use(Router)
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
